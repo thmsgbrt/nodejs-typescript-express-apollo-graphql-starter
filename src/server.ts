3e282bb130ed917e4698907d3fadc48e13ae925b
Thomas Guibert <th.guibert@gmail.com>
2019-11-14 10:04:51 +0100

    Enable playground

--- src/server.ts
+++ src/server.ts
@@ -5,16 +5,17 @@ import { createServer } from 'http';
 import compression from 'compression';
 import cors from 'cors';
 import schema from './schema';
+
 const app = express();
 const server = new ApolloServer({
   schema,
   validationRules: [depthLimit(7)],
+  playground: true,
 });
 app.use('*', cors());
 app.use(compression());
 server.applyMiddleware({ app, path: '/graphql' });
 const httpServer = createServer(app);
-httpServer.listen(
-  { port: 3000 },
-  (): void => console.log(`\n🚀      GraphQL is now running on http://localhost:3000/graphql`)
+httpServer.listen({ port: 3000 }, (): void =>
+  console.log(`\n🚀      GraphQL is now running on http://localhost:3000/graphql`)
 );
